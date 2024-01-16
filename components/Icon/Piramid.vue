7937e6ee852ae421e58c8c591411a8131296df38
David Basilio Espitalher <dbasilioesp@gmail.com>
2024-01-16 10:25:38 -0300

    adicionado icones na section one

---
+++ components/Icon/Piramid.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <svg
+      width="481"
+      height="501"
+      viewBox="0 0 481 501"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M253.963 9.149a63960.346 63960.346 0 0 1 218.128 284.978c9.055 11.685 7.683 19.659-3.978 28.732a26868.348 26868.348 0 0 0-213.19 168.54 20.576 20.576 0 0 1-6.858 4.879 20.536 20.536 0 0 1-16.465 0 20.576 20.576 0 0 1-6.858-4.879A31628.916 31628.916 0 0 0 14.707 324.921c-13.719-10.998-15.64-19.659-4.39-34.093A31716.153 31716.153 0 0 0 224.193 11.623a18.807 18.807 0 0 1 6.133-5.978 18.762 18.762 0 0 1 23.637 3.504Z"
+        stroke="url(#aca)"
+        stroke-width="4.637"
+        stroke-miterlimit="10"
+      />
+      <path
+        d="M240.656 3v492.522"
+        stroke="url(#aba)"
+        stroke-width="4.637"
+        stroke-miterlimit="10"
+      />
+      <defs>
+        <linearGradient
+          id="aca"
+          x1="381.547"
+          y1="131.773"
+          x2="98.219"
+          y2="414.517"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#66286B" />
+          <stop offset="1" stop-color="#E62F72" />
+        </linearGradient>
+        <linearGradient id="aba" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#66286B" />
+          <stop offset="1" stop-color="#E62F72" />
+        </linearGradient>
+      </defs>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    let inverse = 1;
+    const aca = document.querySelector("#aca");
+
+    console.log(aca.getAttribute("x1"), aca.getAttribute("y1"));
+
+    // setInterval(() => {
+    //   let x1 = aca.getAttribute("x1");
+
+    //   if (x1 >= 481) inverse = -1;
+    //   if (x1 <= 0) inverse = 1;
+
+    //   x1 = parseFloat(x1) + inverse * 2;
+
+    //   aca.setAttribute("x1", x1);
+
+    // }, 50);
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,7 @@
     </div>
     <div class="navigation-b">
       <ButtonSet link iconRight="whatsapp"> Contato </ButtonSet>
+      <IconPiramid class="navigation-b__piramid" />
     </div>
   </header>
   <main>
@@ -17,6 +18,7 @@
         <Subtitle class="message-one__subtitle">
           Fortaleça sua presença online e aumente o reconhecimento da sua marca.
         </Subtitle>
+        <IconMouseScroll class="message-one__icon" />
       </div>
     </section>
   </main>
@@ -28,22 +30,62 @@ import "~/assets/styles/index.css";
 </script>
 
 <style>
-section {
+main {
   overflow: hidden;
+}
+
+header {
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%);
+  top: 48px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: var(--container-size);
+  width: 100%;
+  z-index: 10;
+}
+
+.navigation-a {
+  display: flex;
+  gap: 40px;
+  font-family: "Poppins", sans-serif;
+}
+
+.navigation-b {
+}
+
+.navigation-b__piramid {
+  position: absolute;
+  top: 0;
+  right: -390px;
+}
+
+.section-one {
+  background-image: url(assets/section1-bg.jpg);
+  background-size: cover;
+  min-height: 100vh;
 }
 
 .section-one {
   display: flex;
   justify-content: center;
+  position: relative;
 }
 
 .message-one {
   display: flex;
   flex-direction: column;
+  align-items: center;
   gap: 37px;
   margin-block-start: 315px;
   max-width: 640px;
   text-align: center;
   text-wrap: balance;
+}
+
+.message-one__icon {
 }
 </style>
